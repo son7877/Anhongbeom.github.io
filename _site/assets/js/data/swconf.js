db907f4603e5efceec0b70c5d922a0fb965c2ecd
son7877 <hongbeom7877@naver.com>
2024-02-25 21:44:12 +0900

    init

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708864402',resources: [
+    cacheName: 'chirpy-1708865032',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
